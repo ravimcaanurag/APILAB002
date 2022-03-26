a7e0cee41f0c9a306544a29763b3afaf19ece851
Ravi Kumar Ranga <ravimcaanurag@gmail.com>
2022-03-26 15:00:48 +0530

    initial Commit

---
+++ APILAB002/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using APILAB002.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILAB002.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        public readonly courseService ser;
+        public CoursesController(courseService _ser)
+        {
+            ser = _ser;
+        }
+
+        [HttpGet]
+        public IActionResult ListCourses()
+        {
+            return new OkObjectResult(ser.getCourses());
+            
+        }
+    }
+}
